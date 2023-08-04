62d02ac82a52588c6a99a168028951075438e123
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2023-08-04 09:55:31 +0300

    FINAL FILES

--- auth.py
+++ auth.py
@@ -8,7 +8,7 @@ from jose import jwt
 
 '''
 Auth0 URL to get JWT
-https://dev-karamsawalha.us.auth0.com/authorize?audience=karamcapstone&response_type=token&client_id=6el7BJBoNh04IO1XBLkrpoRjuYUrB0hC&redirect_uri=https://sample/callback
+https://dev-karamsawalha.us.auth0.com/authorize?audience=karamcapstone&response_type=token&client_id=6el7BJBoNh04IO1XBLkrpoRjuYUrB0hC&redirect_uri=https://karamcapstoneproject.onrender.com
 
 '''
 
