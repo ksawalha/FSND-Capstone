2f0de1edf38f5b22e3980df849a215fa6e6395ae
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2023-08-04 09:43:07 +0300

    FINAL FILES

--- auth.py
+++ auth.py
@@ -12,30 +12,19 @@ https://dev-karamsawalha.us.auth0.com/authorize?audience=karamcapstone&response_
 
 '''
 
-# Authentication as an ENV variables
+# Authentication variables as an ENV variables
 AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
 ALGORITHMS = [os.environ['ALGORITHMS']]
 API_AUDIENCE = os.environ['API_AUDIENCE']
 AUTH0_ISSUER = f'https://{AUTH0_DOMAIN}/'
 
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-'''
-it should attempt to get the header from the request
-it should raise an AuthError if no header is present
-it should attempt to split bearer and the token
-it should raise an AuthError if the header is malformed
-return the token part of the header
-'''
-def get_token_auth_header():
+def get_token_auth_header():     #checking header and getting token
     auth_header = request.headers.get('Authorization', None)
     if auth_header is None:
         raise AuthError(
@@ -57,16 +46,8 @@ def get_token_auth_header():
     return auth_header_parts[1]
 
 
-'''
-@INPUTS
-    permission: string permission (i.e. 'post:drink')
-    payload: decoded jwt payload
 
-it should raise an AuthError if permissions are not included in the payload
-it should raise an AuthError if the requested permission string is not in the payload permissions array
-return true otherwise
-'''
-def check_permissions(permission, payload):
+def check_permissions(permission, payload):   #Checking payload permissions
     permissions = payload.get('permissions', None)
     if permissions is None:
         raise AuthError(
@@ -87,17 +68,8 @@ def check_permissions(permission, payload):
     return True
 
 
-'''
-@INPUTS
-    token: a json web token (string)
 
-it should be an Auth0 token with key id (kid)
-it should verify the token using Auth0 /.well-known/jwks.json
-it should decode the payload from the token
-it should validate the claims
-return the decoded payload
-'''
-def verify_decode_jwt(token):
+def verify_decode_jwt(token):        #Verifying Decoded JWT info
     try:
         unverified_headers = jwt.get_unverified_headers(token)
         kid = unverified_headers.get('kid', None)
@@ -173,15 +145,7 @@ def verify_decode_jwt(token):
     )
 
 
-'''
-@INPUTS
-    permission: string permission (i.e. 'post:drink')
 
-it should use the get_token_auth_header method to get the token
-it should use the verify_decode_jwt method to decode the jwt
-it should use the check_permissions method validate claims and check the requested permission
-return the decorator which passes the decoded payload to the decorated method
-'''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
